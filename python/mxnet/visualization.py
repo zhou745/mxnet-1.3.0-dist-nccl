02cb2ad9f0926c26e1a9e7988754feb95b293654
MultiLi <litian9308@gmail.com>
2017-07-14 16:56:23 -0700

    Get rid of annoying parameters of BatchNorm layer

--- python/mxnet/visualization.py
+++ python/mxnet/visualization.py
@@ -240,6 +240,14 @@ def plot_network(symbol, title="plot", save_format='pdf', shape=None, node_attrs
             return True
         if name.endswith("_bias"):
             return True
+        if name.endswith("_var"):
+            return True
+        if name.endswith("_beta"):
+            return True
+        if name.endswith("_gamma"):
+            return True
+        if name.endswith("_mean"):
+            return True
         return False
 
     # make nodes
